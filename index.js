1343235f84faf37b7bb02d14093484296a574f6b
johnkennedyb <113602917+johnkennedyb@users.noreply.github.com>
2024-07-10 20:20:15 -1200

    commit

--- index.js
+++ index.js
@@ -1,55 +1,121 @@
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const FormDataModel = require ('./models/FormData');
-
+const crypto = require('crypto');
+const { MailerSend, EmailParams, Sender, Recipient } = require('mailersend');
+const FormDataModel = require('./models/FormData');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://deepcodesystems:deepcodesystems@cluster0.8jzadee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://deepcodesystems:deepcodesystems@cluster0.8jzadee.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-app.post('/register', (req, res)=>{
-    // To post / insert data into database
+const mailerSend = new MailerSend({
+    apiKey: process.env.MAILERSEND_API_KEY,
+});
 
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
+const sentFrom = new Sender("admin@trial-v69oxl59ezzg785k.mlsender.net", "john");
+
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await FormDataModel.findOne({ email });
+        if (user) {
+            res.json("Already registered");
+        } else {
+            const newUser = new FormDataModel(req.body);
+            await newUser.save();
+            res.json(newUser);
         }
-        else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-app.post('/login', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await FormDataModel.findOne({ email });
+        if (user) {
+            if (user.password === password) {
                 res.json("Success");
-            }
-            else{
+            } else {
                 res.json("Wrong password");
             }
+        } else {
+            res.json("No records found!");
         }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+app.post('/request-password-reset', async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await FormDataModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json("User not found");
         }
-    })
-})
+
+        const token = crypto.randomBytes(32).toString('hex');
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        await user.save();
+
+        const resetLink = `http://yourfrontendurl.com/reset-password?token=${token}`;
+
+        console.log("Password reset token generated:", token);
+
+        const recipients = [new Recipient(email, "User")];
+        const emailParams = new EmailParams()
+            .setFrom(sentFrom)
+            .setTo(recipients)
+            .setReplyTo(sentFrom)
+            .setSubject("Password Reset")
+            .setHtml(`
+                <p>You are receiving this because you (or someone else) have requested the reset of the password for your account.</p>
+                <p>Please click on the following link to reset your password:</p>
+                <p><a href="${resetLink}">${resetLink}</a></p>
+                <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
+            `);
+
+        await mailerSend.email.send(emailParams);
+        res.json("Password reset email sent");
+    } catch (err) {
+        console.error("Error sending email:", err);
+        res.status(500).json("Error sending email");
+    }
+});
+
+
+app.post('/reset-password', async (req, res) => {
+    const { token, newPassword } = req.body;
+    try {
+        const user = await FormDataModel.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpires: { $gt: Date.now() },
+        });
+        if (!user) {
+            return res.status(400).json("Password reset token is invalid or has expired");
+        }
+
+        user.password = newPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        await user.save();
+        res.json("Password has been reset");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 app.listen(3001, () => {
-    console.log("Server listining on http://127.0.0.1:3001");
-
+    console.log("Server listening on http://127.0.0.1:3001");
 });
--- models/FormData.js
+++ models/FormData.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const FormDataSchema = new mongoose.Schema({
     name : String,
     email: String,
-    password: String
+    password: String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
 })
 
 const FormDataModel = mongoose.model('log_reg_form', FormDataSchema);
